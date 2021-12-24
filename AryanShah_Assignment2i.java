4b1c0af42225462a4f1b5d63ddbba2ca78cdd27f
aryan1410200 <96378401+aryan1410200@users.noreply.github.com>
2021-12-24 16:43:39 +0530

    Add files via upload

---
+++ AryanShah_Assignment2i.java
@@ -0,0 +1,15 @@
+public class AryanShah_Assignment2i
+{  
+    public static void main(String[] args) 
+    {  
+        //Initialize array  
+        int [] arr = new int [] {1, 2, 3, 4, 5};  
+        int sum = 0;  
+        //Loop through the array to calculate sum of elements  
+        for (int i = 0; i < arr.length; i++) 
+        {  
+           sum = sum + arr[i];  
+        }  
+        System.out.println(sum);  
+    }  
+}  
